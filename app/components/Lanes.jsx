a3e004a4c6160c62c3906db4c44bb228243d1415
GimYoungPhil <youngphil.gim@gmail.com>
2016-05-05 13:14:08 +0900

    add ch6.2

--- app/components/Lanes.jsx
+++ app/components/Lanes.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Lane from './Lane.jsx';
 
-export default class Lanes extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Lanes Pages</h1>
-      </div>
-    )
-  }
+export default ({lanes}) => {
+  return (
+    <div className="lanes">
+      {lanes.map(lane =>
+        <Lane className="lane" key={lane.id} lane={lane}/>
+      )}
+    </div>
+  );
 }
--- doc/ch6.md
+++ doc/ch6.md
@@ -1,3 +1,5 @@
 # 6 From Notes to Kanban
 
 ## 6.1 Extracting `Lanes`
+
+## 6.2 Modeling `Lane`
